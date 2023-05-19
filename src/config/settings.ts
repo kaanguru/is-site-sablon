6e06d129074d7d69ceef694d985fa10c1c7b3934
kaanguru <kaanguru@mail.ru>
2023-05-19 10:07:21 +0300

    urlencode and astro-icon

---
+++ src/config/settings.ts
@@ -0,0 +1,17 @@
+interface Settings {
+	title: string;
+	description: string;
+	url: string;
+	name: string;
+	adres: string;
+	analytics: string;
+  }
+const settings:Settings = {
+	title: `ŞİRKET Sitesi`,
+	description: `Temiz bir web şablonu`,
+	url: `https://cemkaan.com`,
+	name: `Şirket`,
+	adres: `Voyvoda Caddesi (Bankalar Caddesi) No: 35-37 Karaköy Beyoğlu İstanbul`,
+	analytics: "" // analytics code
+};
+export default settings;
